dc6003a2273a1131c21acfe2d4235b1b82a03b5a
da1vidd <156134388+da1vidd@users.noreply.github.com>
2024-11-10 10:37:28 -0700

    Update pyUi.py

--- pyUi.py
+++ pyUi.py
@@ -1,21 +1,24 @@
 import pygame as pyg
 import sys
 import main
-
 pyg.init()
 
 screen = pyg.display.set_mode((600, 600))
 background = pyg.image.load("UiDesign1.png").convert_alpha()
 backgroundRect = background.get_rect(topleft=(0, 0))
+getBoundsRect = pyg.Rect(160, 450, 280, 60) 
 
-playRect = pyg.Rect(160, 300, 280, 60) # 160, 300, 280, 60
-pauseRect = pyg.Rect(160, 380, 280, 60) # 160, 380, 280, 60
+playRect = pyg.Rect(160, 300, 280, 60)
+pauseRect = pyg.Rect(160, 380, 280, 60) 
 
 playButton = pyg.Surface((150, 50), pyg.SRCALPHA)  # Transparent button surface
 playButton.fill((0, 0, 0, 0)) 
 
 pauseButton = pyg.Surface((150, 50), pyg.SRCALPHA)
-pauseButton.fill((0, 0, 0, 0)) 
+pauseButton.fill((0, 0, 0, 0))
+
+getBoundsButton = pyg.Surface((150, 50), pyg.SRCALPHA)
+getBoundsButton.fill((0, 0, 0, 0)) 
 
 running = True
 running_main = False
@@ -29,12 +32,9 @@ while running:
                 running_main = True
             elif pauseRect.collidepoint(event.pos) and running_main:
                 running = False
+            elif getBoundsRect.collidepoint(event.pos):
+                print("pressed bounds button")
 
     screen.blit(background, backgroundRect)
 
     pyg.display.flip()
-
-
-pyg.quit()
-
-#source myenv/bin/activate
